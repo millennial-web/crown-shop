bae85661afa25997e49fb5500771e52ef85bc3a5
Miguel Bonachea <millennial.training.mx@gmail.com>
2022-10-21 20:19:25 -0500

    refactor categories preview and category components

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,9 +1,21 @@
+import { createSelector } from 'reselect';
 
-export const selectCategoriesMap = (state) => {
-  const categoriesMap = state.categories.categories.reduce((accumulator,category) => {
+const selectCategoriesSlice = (state) => state.categories;
+
+//create a memoized selector, creatSelector takes two inputs, 
+//an array of input selectors and and output selector function 
+//(can receive one or more arguments from input array)
+export const selectCategories = createSelector(
+  [selectCategoriesSlice], 
+  (categoriesSlice) => categoriesSlice.categories //this output selector will only run when categoresSlice has changed
+)
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  //transforms the array of categories and their items into an object with category titles as keys
+  (categories) => categories.reduce((accumulator,category) => {
     const { title, items } = category;
-    accumulator[title.toLowerCase()] = items;
+    accumulator[ title.toLowerCase() ] = items;
     return accumulator;
-  }, {});
-  return categoriesMap;
-}
+  }, {})
+)
