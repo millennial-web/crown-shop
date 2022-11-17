1ca70718a3075489d6bf286c5d6110cba5a04a3c
Miguel Bonachea <millennial.training.mx@gmail.com>
2022-11-16 22:21:35 -0600

    testing stripe

--- src/components/card-payment-form/card-payment-form.component.jsx
+++ src/components/card-payment-form/card-payment-form.component.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { PaymentElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
 import { 
-  selectCartBillingInfo,
+  // selectCartBillingInfo,
 } from "../../store/cart/cart.selector";
 
 import { Button } from "../button/button.component";
@@ -14,8 +14,9 @@ const CardPaymentForm = () =>{
 
   const [isProcessingPayment, setIsProcessingPayment] = useState(false);
   const [paymentErrorMessage, setPaymentErrorMessage] = useState(null);
+  const [paymentStatusMessage, setPaymentStatusMessage] = useState('');
   
-  const cartBillingInfo = useSelector(selectCartBillingInfo);
+  // const cartBillingInfo = useSelector(selectCartBillingInfo);
   
   const paymentHandler = async (e) => {
     e.preventDefault();
@@ -31,9 +32,6 @@ const CardPaymentForm = () =>{
 
     const paymentResult = await stripe.confirmPayment({
       elements, 
-      billing_details: {
-        name: cartBillingInfo ? cartBillingInfo.firstName+' '+cartBillingInfo.lastName : 'Guest',
-      },
       confirmParams: {
         return_url: `${window.location.origin}/checkout`,
       },
@@ -52,25 +50,62 @@ const CardPaymentForm = () =>{
     setIsProcessingPayment(false);
   }
 
+  //check if user was redirected after payment and show status message instead of form
+  useEffect(() => {
+    if (!stripe) {
+      return;
+    }
+    const clientSecret = new URLSearchParams(window.location.search).get("payment_intent_client_secret");
+
+    if (!clientSecret) {
+      return;
+    }
+    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent.status) {
+        case "succeeded":
+          setPaymentStatusMessage("Payment succeeded!");
+          break;
+        case "processing":
+          setPaymentStatusMessage("Your payment is processing.");
+          break;
+        case "requires_payment_method":
+          setPaymentStatusMessage("Your payment was not successful, please try again.");
+          break;
+        default:
+          setPaymentStatusMessage("Something went wrong.");
+          break;
+      }
+    });
+  }, [stripe]);
+
   return (
     <>
-      {paymentErrorMessage && 
+      { paymentStatusMessage  && 
+        <h1>{paymentStatusMessage}</h1>
+      }
+
+      { !paymentStatusMessage && paymentErrorMessage && 
         <div className="payment-error-message">
           {paymentErrorMessage}
         </div>
       }
-      <div className="cc-form-container">
-        { stripe && elements && (
-          <PaymentElement />
-        )}
-      </div>
-      <div className="btns-container">
-        <Button 
-          isLoading={isProcessingPayment || !stripe || !elements} 
-          className="main"
-          onClick={paymentHandler}
-        >Pay Now</Button> 
-      </div>
+
+      { !paymentStatusMessage && (
+        <>
+          <div className="cc-form-container">
+            { stripe && elements && (
+              <PaymentElement />
+            )}
+          </div>
+          <div className="btns-container">
+            <Button 
+              isLoading={isProcessingPayment || !stripe || !elements} 
+              className="main"
+              onClick={paymentHandler}
+            >Pay Now</Button> 
+          </div>
+        </>
+      )}
     </>
   )
 }
